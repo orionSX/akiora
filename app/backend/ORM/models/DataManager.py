c09ccdbe1fa56dd860bb1601da9e1c5016d3212c
Orion <asiskrich@gmail.com>
2025-08-07 20:12:34 +0300

    MongoLayer extended

--- app/backend/ORM/models/DataManager.py
+++ app/backend/ORM/models/DataManager.py
@@ -1,15 +1,58 @@
-from typing import Protocol, Dict, Any, TypeVar, List
+from typing import Protocol, Dict, Any, TypeVar, List, Callable, Awaitable
+from models.Document import Document
 
-D = TypeVar("D", bound="DataManager")
+D = TypeVar("D")
+
+
+class DataManagerError(Exception):
+    def __init__(
+        self,
+        document: Dict[str, Any] | Document = None,
+        data_storage: str = None,
+        details: str = None,
+    ):
+        self.document = document
+        self.data_storage = data_storage
+        self.details = details or "Failed IO"
+
+        doc_info = f"Document: {document}" if document else ""
+        coll_info = f" in storage '{data_storage}'" if data_storage else ""
+        message = f"{self.details}{coll_info}. {doc_info}"
+
+        super().__init__(message)
+
+    pass
+
+
+class ReadFail(DataManagerError):
+    pass
+
+
+class CreateFail(DataManagerError):
+    pass
+
+
+class UpdateFail(DataManagerError):
+    pass
+
+
+class DeleteFail(DataManagerError):
+    pass
+
+
+class BadArgs(DataManagerError):
+    pass
 
 
 class DataManager(Protocol):
+    @classmethod
+    def _get_data_storage(cls) -> str: ...
     @classmethod
     async def get(
         cls, find_query: str | Dict[str, Any], many: bool
     ) -> List[D] | D | None: ...
     @classmethod
-    async def create(cls, create_query: D | Dict[str, Any]) -> D: ...
+    async def create(cls, create_query: D | Dict[str, Any]) -> D | List[D]: ...
     @classmethod
     async def update(
         cls,
@@ -19,3 +62,87 @@ class DataManager(Protocol):
 
     @classmethod
     async def delete(find_query: str | Dict[str, Any], many: bool) -> bool: ...
+
+
+async def manage_crud(
+    *,
+    data_storage: str,
+    many: bool,
+    by_id: bool = False,
+    query: str | Dict[str, Any] | None = None,
+    update_data: Dict[str, Any] | None = None,
+    if_many: Callable[..., Awaitable] | None = None,
+    if_many_by_id: Callable[..., Awaitable] | None = None,
+    if_single: Callable[..., Awaitable] | None = None,
+    if_single_by_id: Callable[..., Awaitable] | None = None,
+):
+    await _validate_manager(
+        data_storage=data_storage,
+        many=many,
+        by_id=by_id,
+        if_many=if_many,
+        id_if_many=if_many_by_id,
+        if_single=if_single,
+        id_if_single=if_single_by_id,
+        query=query,
+        update_data=update_data,
+    )
+    kwargs = {
+        k: v
+        for k, v in [("query", query), ("update_data", update_data)]
+        if v is not None
+    }
+
+    do_single = if_single(**kwargs) if not by_id else if_single_by_id(**kwargs)
+    do_many = if_many(**kwargs) if not by_id else if_many_by_id(**kwargs)
+
+    return await _match(flag=many, do_many=do_many, do_single=do_single)
+
+
+async def _validate_manager(
+    *,
+    data_storage: str,
+    many: bool,
+    by_id: bool,
+    if_many,
+    id_if_many,
+    if_single,
+    id_if_single,
+    query,
+    update_data,
+):
+    if not (query or update_data):
+        raise BadArgs(
+            data_storage=data_storage,
+            details="Makes no sense to run CRUD without find and create",
+        )
+    if by_id and not (id_if_many or id_if_single):
+        raise BadArgs(
+            data_storage=data_storage,
+            details="Manager doesnt support id query",
+        )
+    if many and not (if_many or id_if_many):
+        raise BadArgs(
+            data_storage=data_storage,
+            details="Manager doesnt support many query",
+        )
+    if not many and not (if_single or id_if_single):
+        raise BadArgs(
+            data_storage=data_storage,
+            details="Manager doesnt support single query",
+        )
+
+
+async def _match(
+    *,
+    flag: bool,
+    do_many: Callable[..., Awaitable],
+    do_single: Callable[..., Awaitable],
+):
+    match flag:
+        case True:
+            return await do_many
+        case False:
+            return await do_single
+        case _:
+            raise BadArgs(details=f"flag should be bool {flag=}")
---
+++ app/backend/ORM/models/data_managers/MongoManager.py
@@ -0,0 +1,262 @@
+from db_clients import _get_mongo
+from fastapi import Depends
+from typing import TypeVar, Dict, Any, List, Type, Generic
+from motor.motor_asyncio import AsyncIOMotorDatabase, AsyncIOMotorCollection
+from bson import ObjectId
+from models.DataManager import CreateFail, manage_crud, BadArgs
+from models.Document import Document
+
+T = TypeVar("T", bound=Document)
+
+
+class MongoManager(Generic[T]):
+    _model_class: Type[T]
+
+    @classmethod
+    async def get(
+        cls,
+        query: str | ObjectId | Dict[str, Any] | List[str | ObjectId],
+        many: bool = False,
+        by_id: bool = False,
+    ) -> List[T] | T | None:
+        return await manage_crud(
+            data_storage=cls._get_data_storage(),
+            many=many,
+            by_id=by_id,
+            query=query,
+            if_many=cls._get_many,
+            if_many_by_id=cls._get_many_by_id,
+            if_single=cls._get_one,
+            if_single_by_id=cls._get_one_by_id,
+        )
+
+    @classmethod
+    async def create(
+        cls,
+        update_data: T | Dict[str, Any] | List[T] | List[Dict[str, Any]],
+        many: bool = False,
+    ) -> T | List[T]:
+        return await manage_crud(
+            data_storage=cls._get_data_storage(),
+            many=many,
+            update_data=update_data,
+            if_many=cls._create_many,
+            if_single=cls._create_one,
+        )
+
+    @classmethod
+    async def update(
+        cls,
+        query: str | ObjectId | Dict[str, Any] | List[str | ObjectId],
+        update_data: T | Dict[str, Any],
+        many: bool = False,
+        by_id: bool = False,
+    ) -> bool:
+        return await manage_crud(
+            data_storage=cls._get_data_storage(),
+            many=many,
+            by_id=by_id,
+            query=query,
+            update_data=update_data,
+            if_many=cls._update_many,
+            if_many_by_id=cls._update_many_by_id,
+            if_single=cls._update_one,
+            if_single_by_id=cls._update_one_by_id,
+        )
+
+    @classmethod
+    async def delete(
+        cls,
+        query: str | ObjectId | Dict[str, Any] | List[str | ObjectId],
+        many: bool = False,
+        by_id: bool = False,
+    ) -> bool:
+        return await manage_crud(
+            data_storage=cls._get_data_storage(),
+            many=many,
+            by_id=by_id,
+            query=query,
+            if_many=cls._delete_many,
+            if_many_by_id=cls._delete_many_by_id,
+            if_single_by_id=cls._delete_one_by_id,
+            if_single=cls._delete_one,
+        )
+
+    @classmethod
+    def _get_data_storage(cls) -> str:
+        return f"MongoDB : collection = {(cls._model_class.__name__.lower() + 's',)}"
+
+    @classmethod
+    async def _get_collection(
+        cls,
+        db: AsyncIOMotorDatabase = Depends(_get_mongo),
+    ) -> AsyncIOMotorCollection:
+        if not hasattr(cls, "_model_class"):
+            raise RuntimeError("Model class not bound to this manager")
+        return db[cls._model_class.__name__.lower() + "s"]
+
+    @classmethod
+    async def _create_many(
+        cls,
+        update_data: List[T] | List[Dict[str, Any]],
+        collection: AsyncIOMotorCollection = Depends(_get_collection),
+    ) -> List[T]:
+        for doc in update_data:
+            if isinstance(doc, Document):
+                doc = doc.model_dump(exclude={"id"})
+        result = await collection.insert_many(update_data)
+        if result.inserted_ids:
+            return await cls._get_many_by_id(result.inserted_id)
+        raise CreateFail(
+            data_storage=cls._get_data_storage(),
+            document=update_data,
+            details="MongoDB failed creating many",
+        )
+
+    @classmethod
+    async def _create_one(
+        cls,
+        update_data: T | Dict[str, Any],
+        collection: AsyncIOMotorCollection = Depends(_get_collection),
+    ) -> T:
+        if isinstance(update_data, Document):
+            update_data = update_data.model_dump(exclude={"id"})
+        result = await collection.insert_one(update_data)
+        if result.inserted_id:
+            return await cls._get_one_by_id(str(result.inserted_id))
+        raise CreateFail(
+            data_storage=cls._get_data_storage(),
+            document=update_data,
+            details="MongoDB failed creating one",
+        )
+
+    @classmethod
+    async def _update_many(
+        cls,
+        query: Dict[str, Any],
+        update_data: T | Dict[str, Any],
+        collection: AsyncIOMotorCollection = Depends(_get_collection),
+    ) -> bool:
+        if isinstance(update_data, Document):
+            update_data = update_data.model_dump(exclude={"id"})
+        result = await collection.update_many(query, {"$set": update_data})
+        return result.modified_count == result.modified_count
+
+    @classmethod
+    async def _update_many_by_id(
+        cls,
+        query: List[str | ObjectId],
+        update_data: T | Dict[str, Any],
+        collection: AsyncIOMotorCollection = Depends(_get_collection),
+    ) -> bool:
+        query = [x if isinstance(x, ObjectId) else ObjectId(x) for x in query]
+        if isinstance(update_data, Document):
+            update_data = update_data.model_dump(exclude={"id"})
+        result = await collection.update_many(
+            {"_id": {"$in": query}}, {"$set": update_data}
+        )
+        return result.modified_count == result.modified_count
+
+    @classmethod
+    async def _update_one(
+        cls,
+        query: Dict[str, Any],
+        update_data: T | Dict[str, Any],
+        collection: AsyncIOMotorCollection = Depends(_get_collection),
+    ) -> bool:
+        if isinstance(update_data, Document):
+            update_data = update_data.model_dump(exclude={"id"})
+        result = await collection.update_one(query, {"$set": update_data})
+        return result.modified_count == result.modified_count
+
+    @classmethod
+    async def _update_one_by_id(
+        cls,
+        query: str | ObjectId,
+        update_data: T | Dict[str, Any],
+        collection: AsyncIOMotorCollection = Depends(_get_collection),
+    ) -> bool:
+        query = query if isinstance(query, ObjectId) else ObjectId(query)
+        if isinstance(update_data, Document):
+            update_data = update_data.model_dump(exclude={"id"})
+        result = await collection.update_one({"_id": query}, {"$set": update_data})
+        return result.modified_count == result.matched_count
+
+    @classmethod
+    async def _get_one(
+        cls,
+        query: Dict[str, Any],
+        collection: AsyncIOMotorCollection = Depends(_get_collection),
+    ) -> T | None:
+        return await collection.find_one(query)
+
+    @classmethod
+    async def _get_one_by_id(
+        cls,
+        query: str | ObjectId,
+        collection: AsyncIOMotorCollection = Depends(_get_collection),
+    ) -> T | None:
+        query = query if isinstance(query, ObjectId) else ObjectId(query)
+        return await collection.find_one({"_id": query})
+
+    @classmethod
+    async def _get_many(
+        cls,
+        query: Dict[str, Any],
+        collection: AsyncIOMotorCollection = Depends(_get_collection),
+    ) -> List[T]:
+        return await collection.find(query).to_list()
+
+    @classmethod
+    async def _get_many_by_id(
+        cls,
+        query: List[str | ObjectId],
+        collection: AsyncIOMotorCollection = Depends(_get_collection),
+    ) -> List[T] | None:
+        query = [x if isinstance(x, ObjectId) else ObjectId(x) for x in query]
+
+        return await collection.find({"_id": {"$in": query}}).to_list()
+
+    @classmethod
+    async def _delete_one(
+        cls,
+        query: Dict[str, Any],
+        collection: AsyncIOMotorCollection = Depends(_get_collection),
+    ) -> bool:
+        result = await collection.delete_one(query)
+        return result.deleted_count > 0
+
+    @classmethod
+    async def _delete_one_by_id(
+        cls,
+        query: str | ObjectId,
+        collection: AsyncIOMotorCollection = Depends(_get_collection),
+    ) -> bool:
+        query = query if isinstance(query, ObjectId) else ObjectId(query)
+        result = await collection.delete_one({"_id": query})
+        return result.deleted_count > 0
+
+    @classmethod
+    async def _delete_many(
+        cls,
+        query: Dict[str, Any],
+        collection: AsyncIOMotorCollection = Depends(_get_collection),
+    ) -> bool:
+        """do i need to check length of found docs first?"""
+
+        result = await collection.delete_many(query)
+        return result.deleted_count > 0
+
+    @classmethod
+    async def _delete_many_by_id(
+        cls,
+        query: List[str | ObjectId],
+        collection: AsyncIOMotorCollection = Depends(_get_collection),
+    ) -> bool:
+        """do i need to check length of found docs first?"""
+
+        query = [x if isinstance(x, ObjectId) else ObjectId(x) for x in query]
+
+        result = await collection.delete_many({"_id": {"$in": query}})
+
+        return result.deleted_count > 0
